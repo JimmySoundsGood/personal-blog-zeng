60ddb3f869d82309809d0d767bd3c142d9510727
Jimmy <hu37656@outlook.com>
2025-01-18 16:58:28 +0800

    feat: 新增头像

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
+
+const BASE_URL = '/personal-blog-zeng/'; // 争议解决律师
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "My Awesome Project",
-  description: "A VitePress Site",
+  lang: 'zh-CN',
+  base: BASE_URL,
+  title: '个人博客',
+  description: '商事争议解决律师',
+  lastUpdated: true,
+  head: [
+    ['link', { rel: 'icon', href: `${BASE_URL}favicon.ico` }], // 修改了 base 这里也需要同步修改
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Examples', link: '/markdown-examples' },
     ],
 
     sidebar: [
@@ -16,13 +24,13 @@ export default defineConfig({
         text: 'Examples',
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+    ],
+  },
+});
